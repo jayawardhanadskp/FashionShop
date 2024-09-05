6c1817d0d9d0c44acad9a761cadd3fc3e8449ede
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-09-05 13:50:28 +0530

    fonts

---
+++ shopping_app/lib/utils/fonts.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shopping_app/utils/colors.dart';
+
+class AppFonts {
+  static TextStyle normal = GoogleFonts.poppins(fontSize: 20, );
+  static TextStyle bodyText1white = GoogleFonts.poppins(fontSize: 16, color: Colors.white );
+  static TextStyle bodyText1pink = GoogleFonts.poppins(fontSize: 16, color: AppColors.pink);
+}
