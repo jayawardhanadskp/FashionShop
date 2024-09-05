f3553bb2e7c3e0fb5e957eb1fb18e25a4b5d393b
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-09-05 13:52:31 +0530

    main

---
+++ shopping_app/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+import 'package:shopping_app/users/authentication/login_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+        useMaterial3: true,
+      ),
+      home: const LoginScreen()
+    );
+  }
+}
