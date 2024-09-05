5f051d9bf6900ce395891f46c9ae1f08b825f061
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-09-05 13:50:21 +0530

    colors

---
+++ shopping_app/lib/utils/colors.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color pink = Color(0xFFF389B9);
+  static const Color black = Color(0x00ff0000);
+  static Color white = Colors.white;
+}
